af02bc41ab424eb7ba9f3348eab06e9356740718
Brandon <thebdank@gmail.com>
2017-12-12 07:34:59 -0500

    refining async work

--- async/cascading-select-location.js
+++ async/cascading-select-location.js
@@ -1,31 +1,76 @@
 "use strict";
 
 (function() {
+  var timeNow = Date.now();
+  var timeNow2;
   var countryDDM = $("#country");
   var statesDDM = $("#state");
+  var citiesDDM = $("#city");
   var countryURL =
     "https://rawgit.com/alucidwolf/alucidwolf.github.io/master/json/countries.json";
   var statesURL =
     "https://rawgit.com/alucidwolf/alucidwolf.github.io/master/json/canada-cities.json";
-  countryDDM.append(
-    '<option selected=""true"" disabled>Choose Country</option>'
-  );
+  var bigCityURL =
+    "https://rawgit.com/alucidwolf/alucidwolf.github.io/master/json/global-cities.json";
+  countryDDM.append('<option selected="true" disabled>Choose Country</option>');
   countryDDM.prop("selectedIndex", 0);
 
-  function resolveAfter2Seconds(x) {
-    return new Promise((resolve, reject) => {});
+  const makeRequest = async () => {
+    const data = await getJSON();
+    const moreData = await makeAnotherRequest();
+  };
+
+  function getJSON() {
+    $.getJSON(countryURL, data => {
+      $.each(data, (key, entry) => {
+        countryDDM.append(
+          $("<option></option>")
+            .attr("value", entry.code)
+            .text(entry.name)
+        );
+      });
+      return data;
+    });
   }
 
-  async function add1(x) {
-    var timeNow = Date();
-    console.log(timeNow);
-    const a = await resolveAfter2Seconds(20);
-    const b = await resolveAfter2Seconds(30);
-    return x + a + b;
+  function makeAnotherRequest() {
+    $.getJSON(statesURL, data => {
+      $.each(data, (key, entry) => {
+        statesDDM.append(
+          $("<option></option>")
+            .attr("value", entry.state)
+            .text(entry.name)
+        );
+      });
+      return data;
+    });
   }
 
-  add1(10).then(v => {
-    var timeNow = Date();
-    console.log(v + " " + timeNow); // prints 60 after 4 seconds.
-  });
+  //makeRequest();
+
+  //
+
+  //getData("https://api.icndb.com/jokes/random");
+  getData(bigCityURL);
+
+  async function getData(url) {
+    const dataset = await $.ajax(url);
+    // document.querySelector(".joke").innerHTML = dataset.value.joke;
+    //document.querySelector(".joke").innerHTML = dataset[1].name;
+    $.each(dataset, (key, entry) => {
+      if (entry.country === "AD") {
+        citiesDDM.append(
+          $("<option></option>")
+            .attr("value", entry.country)
+            .text(entry.name)
+        );
+      }
+    });
+    if (dataset[0].name != 0) {
+      timeNow2 = Date.now();
+      console.log(timeNow2 - timeNow);
+    }
+    // if (dataset.type === "success") {
+    // }
+  }
 })();
