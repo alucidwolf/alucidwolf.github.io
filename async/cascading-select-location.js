779a282338a9791384cd4b3aaf7740ce271aa541
Brandon <thebdank@gmail.com>
2017-12-13 16:15:03 -0500

    more work on async for cascading dropdown

--- async/cascading-select-location.js
+++ async/cascading-select-location.js
@@ -3,6 +3,7 @@
 (function() {
   var timeNow = Date.now();
   var timeNow2;
+  var count = 0;
   var countryDDM = $("#country");
   var statesDDM = $("#state");
   var citiesDDM = $("#city");
@@ -51,25 +52,54 @@
   //
 
   //getData("https://api.icndb.com/jokes/random");
-  getData(bigCityURL);
-
-  async function getData(url) {
-    const dataset = await $.ajax(url);
-    // document.querySelector(".joke").innerHTML = dataset.value.joke;
-    //document.querySelector(".joke").innerHTML = dataset[1].name;
-    $.each(dataset, (key, entry) => {
-      if (entry.country === "AD") {
+  //getData(bigCityURL);
+  getData(bigCityURL)
+    .then(result => {
+      let newResult = [];
+      $.each(result, (key, entry) => {
+        if (entry.country === "ES") {
+          newResult.push(entry);
+          count++;
+        }
+      });
+      return newResult;
+    })
+    .then(result => {
+      console.log(result);
+      $.each(result, (key, entry) => {
         citiesDDM.append(
           $("<option></option>")
             .attr("value", entry.country)
             .text(entry.name)
         );
+      });
+      $("#city").css("background", "#fff");
+      if (result[0].name != 0) {
+        timeNow2 = Date.now();
+        console.log(
+          "time: " + (timeNow2 - timeNow) + ", Number of items: " + count
+        );
       }
     });
-    if (dataset[0].name != 0) {
-      timeNow2 = Date.now();
-      console.log(timeNow2 - timeNow);
-    }
+
+  async function getData(url) {
+    console.log("start");
+    $("#city").css("background", "#f90");
+    const dataset = await $.getJSON(url);
+    //console.log(dataset);
+    // document.querySelector(".joke").innerHTML = dataset.value.joke;
+    //document.querySelector(".joke").innerHTML = dataset[1].name;
+    // $.each(dataset, (key, entry) => {
+    //   if (entry.country === "CN") {
+    //     citiesDDM.append(
+    //       $("<option></option>")
+    //         .attr("value", entry.country)
+    //         .text(entry.name)
+    //     );
+    //     count++;
+    //   }
+    // });
+    return dataset;
     // if (dataset.type === "success") {
     // }
   }
