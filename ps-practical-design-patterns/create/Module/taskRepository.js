310f2b9e0ae10660db71148b4832513c0cbbec35
Brandon <brandonfranklin@Macintosh.local>
2018-06-04 18:25:35 -0400

    missing stuff

--- ps-practical-design-patterns/create/Module/taskRepository.js
+++ ps-practical-design-patterns/create/Module/taskRepository.js
@@ -1,25 +1,26 @@
-var repo = function () {
+var repo = function() {
+  // keep db connections private to this function
+  var db = {};
 
-    var db = {};
-
-    var get = function (id) {
-        console.log('Getting task ' + id);
-        return {
-            name: 'new task from db'
-        }
-    }
-
-    var save = function (task) {
-        console.log('Saving ' + task.name + ' to the db');
-    }
-    
-    
+  // operate on data returned from db
+  var get = function(id) {
+    console.log("Getting task " + id);
     return {
-        get: get,
-        save: save
-    }
+      name: "new task from db"
+    };
+  };
 
+  // operate on data returned from db
+  var save = function(task) {
+    console.log("Saving " + task.name + " to the db");
+  };
 
-}
+  // return methods like this for easy reference aka revealing
+  return {
+    get: get,
+    save: save
+  };
+};
 
+// execute on export has advantages
 module.exports = repo();
