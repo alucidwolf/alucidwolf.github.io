11583d60c97a78c3840a33e1d23998b9bd60599d
Brandon <alucidwolf@users.noreply.github.com>
2018-08-08 11:37:08 -0400

    Update fake-datatable.js

--- fake-datatable.js
+++ fake-datatable.js
@@ -1,288 +1,345 @@
 {
-	"data": [{
-		"id": "1",
-		"name": "Tiger Nixon",
-		"position": "System Architect",
-		"office": "Edinburgh"
-	}, {
-		"id": "2",
-		"name": "Garrett Winters",
-		"position": "Accountant",
-		"office": "Tokyo"
-	}, {
-		"id": "3",
-		"name": "Ashton Cox",
-		"position": "Junior Technical Author",
-		"office": "San Francisco"
-	}, {
-		"id": "4",
-		"name": "Cedric Kelly",
-		"position": "Senior Javascript Developer",
-		"office": "Edinburgh"
-	}, {
-		"id": "5",
-		"name": "Airi Satou",
-		"position": "Accountant",
-		"office": "Tokyo"
-	}, {
-		"id": "6",
-		"name": "Brielle Williamson",
-		"position": "Integration Specialist",
-		"office": "New York"
-	}, {
-		"id": "7",
-		"name": "Herrod Chandler",
-		"position": "Sales Assistant",
-		"office": "San Francisco"
-	}, {
-		"id": "8",
-		"name": "Rhona Davidson",
-		"position": "Integration Specialist",
-		"office": "Tokyo"
-	}, {
-		"id": "9",
-		"name": "Colleen Hurst",
-		"position": "Javascript Developer",
-		"office": "San Francisco"
-	}, {
-		"id": "10",
-		"name": "Sonya Frost",
-		"position": "Software Engineer",
-		"office": "Edinburgh"
-	}, {
-		"id": "11",
-		"name": "Jena Gaines",
-		"position": "Office Manager",
-		"office": "London"
-	}, {
-		"id": "12",
-		"name": "Quinn Flynn",
-		"position": "Support Lead",
-		"office": "Edinburgh"
-	}, {
-		"id": "13",
-		"name": "Charde Marshall",
-		"position": "Regional Director",
-		"office": "San Francisco"
-	}, {
-		"id": "14",
-		"name": "Haley Kennedy",
-		"position": "Senior Marketing Designer",
-		"office": "London"
-	}, {
-		"id": "15",
-		"name": "Tatyana Fitzpatrick",
-		"position": "Regional Director",
-		"office": "London"
-	}, {
-		"id": "16",
-		"name": "Michael Silva",
-		"position": "Marketing Designer",
-		"office": "London"
-	}, {
-		"id": "17",
-		"name": "Paul Byrd",
-		"position": "Chief Financial Officer (CFO)",
-		"office": "New York"
-	}, {
-		"id": "18",
-		"name": "Gloria Little",
-		"position": "Systems Administrator",
-		"office": "New York"
-	}, {
-		"id": "19",
-		"name": "Bradley Greer",
-		"position": "Software Engineer",
-		"office": "London"
-	}, {
-		"id": "20",
-		"name": "Dai Rios",
-		"position": "Personnel Lead",
-		"office": "Edinburgh"
-	}, {
-		"id": "21",
-		"name": "Jenette Caldwell",
-		"position": "Development Lead",
-		"office": "New York"
-	}, {
-		"id": "22",
-		"name": "Yuri Berry",
-		"position": "Chief Marketing Officer (CMO)",
-		"office": "New York"
-	}, {
-		"id": "23",
-		"name": "Caesar Vance",
-		"position": "Pre-Sales Support",
-		"office": "New York"
-	}, {
-		"id": "24",
-		"name": "Doris Wilder",
-		"position": "Sales Assistant",
-		"office": "Sidney"
-	}, {
-		"id": "25",
-		"name": "Angelica Ramos",
-		"position": "Chief Executive Officer (CEO)",
-		"office": "London"
-	}, {
-		"id": "26",
-		"name": "Gavin Joyce",
-		"position": "Developer",
-		"office": "Edinburgh"
-	}, {
-		"id": "27",
-		"name": "Jennifer Chang",
-		"position": "Regional Director",
-		"office": "Singapore"
-	}, {
-		"id": "28",
-		"name": "Brenden Wagner",
-		"position": "Software Engineer",
-		"office": "San Francisco"
-	}, {
-		"id": "29",
-		"name": "Fiona Green",
-		"position": "Chief Operating Officer (COO)",
-		"office": "San Francisco"
-	}, {
-		"id": "30",
-		"name": "Shou Itou",
-		"position": "Regional Marketing",
-		"office": "Tokyo"
-	}, {
-		"id": "31",
-		"name": "Michelle House",
-		"position": "Integration Specialist",
-		"office": "Sidney"
-	}, {
-		"id": "32",
-		"name": "Suki Burks",
-		"position": "Developer",
-		"office": "London"
-	}, {
-		"id": "33",
-		"name": "Prescott Bartlett",
-		"position": "Technical Author",
-		"office": "London"
-	}, {
-		"id": "34",
-		"name": "Gavin Cortez",
-		"position": "Team Leader",
-		"office": "San Francisco"
-	}, {
-		"id": "35",
-		"name": "Martena Mccray",
-		"position": "Post-Sales support",
-		"office": "Edinburgh"
-	}, {
-		"id": "36",
-		"name": "Unity Butler",
-		"position": "Marketing Designer",
-		"office": "San Francisco"
-	}, {
-		"id": "37",
-		"name": "Howard Hatfield",
-		"position": "Office Manager",
-		"office": "San Francisco"
-	}, {
-		"id": "38",
-		"name": "Hope Fuentes",
-		"position": "Secretary",
-		"office": "San Francisco"
-	}, {
-		"id": "39",
-		"name": "Vivian Harrell",
-		"position": "Financial Controller",
-		"office": "San Francisco"
-	}, {
-		"id": "40",
-		"name": "Timothy Mooney",
-		"position": "Office Manager",
-		"office": "London"
-	}, {
-		"id": "41",
-		"name": "Jackson Bradshaw",
-		"position": "Director",
-		"office": "New York"
-	}, {
-		"id": "42",
-		"name": "Olivia Liang",
-		"position": "Support Engineer",
-		"office": "Singapore"
-	}, {
-		"id": "43",
-		"name": "Bruno Nash",
-		"position": "Software Engineer",
-		"office": "London"
-	}, {
-		"id": "44",
-		"name": "Sakura Yamamoto",
-		"position": "Support Engineer",
-		"office": "Tokyo"
-	}, {
-		"id": "45",
-		"name": "Thor Walton",
-		"position": "Developer",
-		"office": "New York"
-	}, {
-		"id": "46",
-		"name": "Finn Camacho",
-		"position": "Support Engineer",
-		"office": "San Francisco"
-	}, {
-		"id": "47",
-		"name": "Serge Baldwin",
-		"position": "Data Coordinator",
-		"office": "Singapore"
-	}, {
-		"id": "48",
-		"name": "Zenaida Frank",
-		"position": "Software Engineer",
-		"office": "New York"
-	}, {
-		"id": "49",
-		"name": "Zorita Serrano",
-		"position": "Software Engineer",
-		"office": "San Francisco"
-	}, {
-		"id": "50",
-		"name": "Jennifer Acosta",
-		"position": "Junior Javascript Developer",
-		"office": "Edinburgh"
-	}, {
-		"id": "51",
-		"name": "Cara Stevens",
-		"position": "Sales Assistant",
-		"office": "New York"
-	}, {
-		"id": "52",
-		"name": "Hermione Butler",
-		"position": "Regional Director",
-		"office": "London"
-	}, {
-		"id": "53",
-		"name": "Lael Greer",
-		"position": "Systems Administrator",
-		"office": "London"
-	}, {
-		"id": "54",
-		"name": "Jonas Alexander",
-		"position": "Developer",
-		"office": "San Francisco"
-	}, {
-		"id": "55",
-		"name": "Shad Decker",
-		"position": "Regional Director",
-		"office": "Edinburgh"
-	}, {
-		"id": "56",
-		"name": "Michael Bruce",
-		"position": "Javascript Developer",
-		"office": "Singapore"
-	}, {
-		"id": "57",
-		"name": "Donna Snider",
-		"position": "Customer Support",
-		"office": "New York"
-	}]
+    "data": [{
+        "id": "1",
+        "name": "Tiger Nixon",
+        "position": "System Architect",
+        "office": "Edinburgh",
+        "status": true
+    }, {
+        "id": "2",
+        "name": "Garrett Winters",
+        "position": "Accountant",
+        "office": "Tokyo",
+        "status": true
+    }, {
+        "id": "3",
+        "name": "Ashton Cox",
+        "position": "Junior Technical Author",
+        "office": "San Francisco",
+        "status": true
+    }, {
+        "id": "4",
+        "name": "Cedric Kelly",
+        "position": "Senior Javascript Developer",
+        "office": "Edinburgh",
+        "status": true
+    }, {
+        "id": "5",
+        "name": "Airi Satou",
+        "position": "Accountant",
+        "office": "Tokyo",
+        "status": true
+    }, {
+        "id": "6",
+        "name": "Brielle Williamson",
+        "position": "Integration Specialist",
+        "office": "New York",
+        "status": true
+    }, {
+        "id": "7",
+        "name": "Herrod Chandler",
+        "position": "Sales Assistant",
+        "office": "San Francisco",
+        "status": true
+    }, {
+        "id": "8",
+        "name": "Rhona Davidson",
+        "position": "Integration Specialist",
+        "office": "Tokyo",
+        "status": true
+    }, {
+        "id": "9",
+        "name": "Colleen Hurst",
+        "position": "Javascript Developer",
+        "office": "San Francisco",
+        "status": true
+    }, {
+        "id": "10",
+        "name": "Sonya Frost",
+        "position": "Software Engineer",
+        "office": "Edinburgh",
+        "status": true
+    }, {
+        "id": "11",
+        "name": "Jena Gaines",
+        "position": "Office Manager",
+        "office": "London",
+        "status": true
+    }, {
+        "id": "12",
+        "name": "Quinn Flynn",
+        "position": "Support Lead",
+        "office": "Edinburgh",
+        "status": true
+    }, {
+        "id": "13",
+        "name": "Charde Marshall",
+        "position": "Regional Director",
+        "office": "San Francisco",
+        "status": true
+    }, {
+        "id": "14",
+        "name": "Haley Kennedy",
+        "position": "Senior Marketing Designer",
+        "office": "London",
+        "status": true
+    }, {
+        "id": "15",
+        "name": "Tatyana Fitzpatrick",
+        "position": "Regional Director",
+        "office": "London",
+        "status": true
+    }, {
+        "id": "16",
+        "name": "Michael Silva",
+        "position": "Marketing Designer",
+        "office": "London",
+        "status": true
+    }, {
+        "id": "17",
+        "name": "Paul Byrd",
+        "position": "Chief Financial Officer (CFO)",
+        "office": "New York",
+        "status": true
+    }, {
+        "id": "18",
+        "name": "Gloria Little",
+        "position": "Systems Administrator",
+        "office": "New York",
+        "status": true
+    }, {
+        "id": "19",
+        "name": "Bradley Greer",
+        "position": "Software Engineer",
+        "office": "London",
+        "status": true
+    }, {
+        "id": "20",
+        "name": "Dai Rios",
+        "position": "Personnel Lead",
+        "office": "Edinburgh",
+        "status": true
+    }, {
+        "id": "21",
+        "name": "Jenette Caldwell",
+        "position": "Development Lead",
+        "office": "New York",
+        "status": true
+    }, {
+        "id": "22",
+        "name": "Yuri Berry",
+        "position": "Chief Marketing Officer (CMO)",
+        "office": "New York",
+        "status": true
+    }, {
+        "id": "23",
+        "name": "Caesar Vance",
+        "position": "Pre-Sales Support",
+        "office": "New York",
+        "status": true
+    }, {
+        "id": "24",
+        "name": "Doris Wilder",
+        "position": "Sales Assistant",
+        "office": "Sidney",
+        "status": true
+    }, {
+        "id": "25",
+        "name": "Angelica Ramos",
+        "position": "Chief Executive Officer (CEO)",
+        "office": "London",
+        "status": true
+    }, {
+        "id": "26",
+        "name": "Gavin Joyce",
+        "position": "Developer",
+        "office": "Edinburgh",
+        "status": true
+    }, {
+        "id": "27",
+        "name": "Jennifer Chang",
+        "position": "Regional Director",
+        "office": "Singapore",
+        "status": true
+    }, {
+        "id": "28",
+        "name": "Brenden Wagner",
+        "position": "Software Engineer",
+        "office": "San Francisco",
+        "status": true
+    }, {
+        "id": "29",
+        "name": "Fiona Green",
+        "position": "Chief Operating Officer (COO)",
+        "office": "San Francisco",
+        "status": true
+    }, {
+        "id": "30",
+        "name": "Shou Itou",
+        "position": "Regional Marketing",
+        "office": "Tokyo",
+        "status": true
+    }, {
+        "id": "31",
+        "name": "Michelle House",
+        "position": "Integration Specialist",
+        "office": "Sidney",
+        "status": true
+    }, {
+        "id": "32",
+        "name": "Suki Burks",
+        "position": "Developer",
+        "office": "London",
+        "status": true
+    }, {
+        "id": "33",
+        "name": "Prescott Bartlett",
+        "position": "Technical Author",
+        "office": "London",
+        "status": true
+    }, {
+        "id": "34",
+        "name": "Gavin Cortez",
+        "position": "Team Leader",
+        "office": "San Francisco",
+        "status": true
+    }, {
+        "id": "35",
+        "name": "Martena Mccray",
+        "position": "Post-Sales support",
+        "office": "Edinburgh",
+        "status": true
+    }, {
+        "id": "36",
+        "name": "Unity Butler",
+        "position": "Marketing Designer",
+        "office": "San Francisco",
+        "status": true
+    }, {
+        "id": "37",
+        "name": "Howard Hatfield",
+        "position": "Office Manager",
+        "office": "San Francisco",
+        "status": true
+    }, {
+        "id": "38",
+        "name": "Hope Fuentes",
+        "position": "Secretary",
+        "office": "San Francisco",
+        "status": true
+    }, {
+        "id": "39",
+        "name": "Vivian Harrell",
+        "position": "Financial Controller",
+        "office": "San Francisco",
+        "status": true
+    }, {
+        "id": "40",
+        "name": "Timothy Mooney",
+        "position": "Office Manager",
+        "office": "London",
+        "status": true
+    }, {
+        "id": "41",
+        "name": "Jackson Bradshaw",
+        "position": "Director",
+        "office": "New York",
+        "status": true
+    }, {
+        "id": "42",
+        "name": "Olivia Liang",
+        "position": "Support Engineer",
+        "office": "Singapore",
+        "status": true
+    }, {
+        "id": "43",
+        "name": "Bruno Nash",
+        "position": "Software Engineer",
+        "office": "London",
+        "status": true
+    }, {
+        "id": "44",
+        "name": "Sakura Yamamoto",
+        "position": "Support Engineer",
+        "office": "Tokyo",
+        "status": true
+    }, {
+        "id": "45",
+        "name": "Thor Walton",
+        "position": "Developer",
+        "office": "New York",
+        "status": true
+    }, {
+        "id": "46",
+        "name": "Finn Camacho",
+        "position": "Support Engineer",
+        "office": "San Francisco",
+        "status": true
+    }, {
+        "id": "47",
+        "name": "Serge Baldwin",
+        "position": "Data Coordinator",
+        "office": "Singapore",
+        "status": true
+    }, {
+        "id": "48",
+        "name": "Zenaida Frank",
+        "position": "Software Engineer",
+        "office": "New York",
+        "status": true
+    }, {
+        "id": "49",
+        "name": "Zorita Serrano",
+        "position": "Software Engineer",
+        "office": "San Francisco",
+        "status": true
+    }, {
+        "id": "50",
+        "name": "Jennifer Acosta",
+        "position": "Junior Javascript Developer",
+        "office": "Edinburgh",
+        "status": true
+    }, {
+        "id": "51",
+        "name": "Cara Stevens",
+        "position": "Sales Assistant",
+        "office": "New York",
+        "status": true
+    }, {
+        "id": "52",
+        "name": "Hermione Butler",
+        "position": "Regional Director",
+        "office": "London",
+        "status": true
+    }, {
+        "id": "53",
+        "name": "Lael Greer",
+        "position": "Systems Administrator",
+        "office": "London",
+        "status": true
+    }, {
+        "id": "54",
+        "name": "Jonas Alexander",
+        "position": "Developer",
+        "office": "San Francisco",
+        "status": true
+    }, {
+        "id": "55",
+        "name": "Shad Decker",
+        "position": "Regional Director",
+        "office": "Edinburgh",
+        "status": true
+    }, {
+        "id": "56",
+        "name": "Michael Bruce",
+        "position": "Javascript Developer",
+        "office": "Singapore",
+        "status": true
+    }, {
+        "id": "57",
+        "name": "Donna Snider",
+        "position": "Customer Support",
+        "office": "New York",
+        "status": true
+    }]
 }
