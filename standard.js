686a98db7cd08e284983ca6ab119f41b6b09e2c9
Brandon <alucidwolf@users.noreply.github.com>
2021-07-23 11:35:54 -0400

    Create standard.js

---
+++ standard.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = {
+    rules: {
+        // 7S Custom Rules
+        "no-console": [
+          "error",
+          {
+            "allow": [
+              "warn",
+              "error"
+            ]
+          }
+        ]
+    },
+};
